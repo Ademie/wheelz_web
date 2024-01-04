0549a0f00a4b0665135235a1d374b4a7fd4e8e9c
ademie <isijolaademie@gmail.com>
2024-01-04 06:05:00 -0600

    displaying driver pages

--- lib/pages/drivers_page.dart
+++ lib/pages/drivers_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wheelz_web/methods/common_methods.dart';
 
 class DriversPage extends StatefulWidget
 {
@@ -10,17 +11,51 @@ class DriversPage extends StatefulWidget
   State<DriversPage> createState() => _DriversPageState();
 }
 
-class _DriversPageState extends State<DriversPage> {
+class _DriversPageState extends State<DriversPage>
+{
+  CommonMethods cMethods = CommonMethods();
+
   @override
   Widget build(BuildContext context)
   {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "DriversPage",
-          style: TextStyle(
-              color: Colors.pinkAccent,
-              fontSize: 24
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  "Manage Drivers",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 18,
+              ),
+
+              Row(
+                children: [
+                  cMethods.header(2, "DRIVER ID"),
+                  cMethods.header(1, "PICTURE"),
+                  cMethods.header(1, "NAME"),
+                  cMethods.header(1, "CAR DETAILS"),
+                  cMethods.header(1, "PHONE"),
+                  cMethods.header(1, "TOTAL EARNINGS"),
+                  cMethods.header(1, "ACTION"),
+                ],
+              ),
+
+              //display data
+
+            ],
           ),
         ),
       ),
